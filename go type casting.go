bff2d717316399b288762bbe809acd942403bc3a
jubayerahmedsamrat <jasamrat@gmail.com>
2023-03-16 11:39:26 +0600

    adding ASCII convertion

--- go type casting.go
+++ go type casting.go
@@ -1,18 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
+	fmt.Println("Go type Casting/ Convertion/ Assertion")
 
-	fmt.Println("Go type casting")
-	
-	var number int = 55
-	fmt.Printf("Value = %v, Type = %T\n", number, number)
+	var number1 int = 55
+	fmt.Printf(" Number1 Value = %v, Type = %T\n", number1, number1)
 
-	var number2 float64 = float64(number)
-	fmt.Printf("Value = %v, Type = %T\n", number2, number2) 
+	var number2 float64 = float64(number1)
+	fmt.Printf(" Number2 Value = %v, Type = %T\n", number2, number2)
 
+	var number3 int = int(number2)
+	fmt.Printf(" Number3 float64 to integer -> Value = %v, Type = %T\n", number3, number3)
 
+	var number4 string = string(number3)
+	fmt.Printf(" Number4 int to string, ASCII value -> Value = %v, Type = %T", number4, number4)
+
+	var number5 string = strconv.Itoa(number3)
+	fmt.Printf(" Number5 ASCII to English -> value = %v, Type = %T", number5, number5)
 
 }
